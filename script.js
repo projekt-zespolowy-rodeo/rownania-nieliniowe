2e44025418f6b0681a593735b6e80599d3c063c2
Kamil Górny <kamil.gorny99@gmail.com>
2021-01-15 19:43:59 +0100

    Update script.js
    Update

--- script.js
+++ script.js
@@ -3,32 +3,43 @@ let calculateButton = document.getElementById("calculate");
 
 function newtonsMethod(func, derivative, epsilon){
     let solution = document.getElementById("solution");
-    // let xn = x0;
+    
     let iterations = 0;
     let przedzial = -5;
     let counter = 20;
+    let output = ''
     for(let i=-47; i<=przedzial; i++){
+        if(i==-47){
+            solution.innerHTML = "";
+        }
         counter = 20;
         let xn = i;
+        iterations = 0;
         while(counter>0){
             let f = func(xn);
             let df = derivative(xn);
+             
+            if (Math.abs(f) <= epsilon){
+                console.log("x0= "+i+" xn= "+xn);
+                let newline = document.createElement("br");
+                let textnode = document.createTextNode("Znaleziono rozwiązanie po: "+iterations+" krokach, przybliżone rozwiązanie to: "+xn+"\n")
+            
+                solution.appendChild(newline);
+                solution.appendChild(textnode);
+                
+                break;
+            }
             if(df == 0){
                 console.log("Zerowa pochodna, nie znaleziono rozwiązania.");
                 break;
     
-            }  
-            if (Math.abs(f) <= epsilon){
-                console.log("x0= "+i+" xn= "+xn);
-                solution.innerHTML = "Znaleziono rozwiązanie po: "+iterations+" krokach, przybliżone rozwiązanie to: "+xn;
-                break;
-            }
-             
+            } 
             iterations+=1;
           
             xn = xn - f/df;
             counter--;
         }
+        
         console.log("wykonano")
     }
    
